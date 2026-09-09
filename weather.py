68b31000f6f0e02fb92d77e42fe7759e8cb09b56
Sarah Watkins <20177109@tafe.wa.edu.au>
2026-09-09 11:41:00 +0800

    Updated CSV code

--- weather.py
+++ weather.py
@@ -9,7 +9,7 @@ def variance(in_series):
 
 def standard_deviation(in_series):
     std_result = variance**0.5
-        return std_result
+    return std_result
 
 def filter_series(year_series, month_series, day_series, data_series, max_date=None, min_date=None):
     pass
@@ -19,8 +19,10 @@ def read_csv(file,default_value=None):
     with open(file) as f:
         lines = f.readlines()
     lines = [line.strip().split(',') for line in lines]
-    for i in range(len(lines[0])):
-        data_table[lines[0][i]] = [default_value if (len(line[i]) == 0) else float(line[i]) for line in lines[1:]]
+    for i in range(1,len(lines[0])):
+        conversion = datetime.fromisoformat if (lines[0][i]=='Date') else float
+        data_table[lines[0][i]] = \
+            [default_value if (len(line[i]) == 0) else conversion(line[i]) for line in lines[1:]]
     return data_table
 
 def get_user_choice(options):
