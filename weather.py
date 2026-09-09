1c5b35a0a9ccf7ed6bc7ea9241c603d84faa9fea
Harshitha <20165944@tafe.wa.edu.au>
2026-09-09 12:41:26 +0800

    Implemented interquartile range calculation

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+import statistics
 
 series_titles = ["Maximum temperature (Degree C)", "Minimum temperature (Degree C)", "Rainfall amount (millimetres)"]
 
@@ -11,7 +12,36 @@ def standard_deviation(in_series):
     pass
 
 def filter_series(year_series, month_series, day_series, data_series, max_date=None, min_date=None):
-    pass
+    filtered = []
+    for y,m, d, value in zip(year_series,
+                            month_series,
+                            day_series,
+                            data_series):
+          current = (y, m, d)
+          if min_date and current < min_date:
+            continue
+    
+          if max_date and current > max_date:
+            continue
+    
+          filtered.append(value)
+    return filtered
+
+def interquartile_range(in_series):
+    data = sorted(in_series)
+    mid = len(data) // 2
+
+    if len(data) % 2 == 0:
+        lower_half = data[:mid]
+        upper_half = data[mid:]
+    else:
+        lower_half = data[:mid]
+        upper_half = data[mid + 1:]
+
+    q1 = statistics.median(lower_half)
+    q3 = statistics.median(upper_half)
+
+    return q3 - q1
 
 def read_csv(file,default_value=None):
     data_table = {}
